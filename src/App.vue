d012570affd57e6ef37b50ee0bb5dddc89f8cdd3
utshaweu <utshaw1995@gmail.com>
2022-02-12 20:22:48 +0600

    naming slot added

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
-  <Person name="Sajib" age="26">
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa quia, iste veniam earum id vel esse pariatur assumenda, quas minima magnam! Nobis quia maxime pariatur ad corrupti porro modi vel incidunt rem iusto ullam tempora ratione nemo laboriosam quis, soluta eveniet fuga iste laborum? Maiores dolor vel doloremque voluptatibus.</p>
-  </Person>
+  <Layout>
+    <h1>Main Content</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt minus modi pariatur dolore tempora facere sint, corporis hic porro et ipsam magni iste. Minus perspiciatis ipsam debitis dolore voluptatum dolor est neque, quibusdam temporibus quas rerum nostrum at consequuntur quasi illo distinctio earum deserunt ullam itaque? Placeat numquam magnam quod.</p>
+      <template #sidebar-left>
+        <h1>Sidebar Left</h1>
+      </template>
+      <template #sidebar-right>
+        <h1>Sidebar Right</h1>
+      </template>
+  </Layout>
 </template>
 
 <script>
 import Person from "./components/Person.vue";
+import Layout from "./components/Layout.vue";
 export default {
   name: "App",
   components: {
    Person, 
+   Layout,
   },
   data() {
     return {
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <header class="flex justify-between p-10">
+    <h1 class="font-semibold text-4xl text-indigo-500">Vue js</h1>
+
+    <nav class="flex space-x-4">
+      <a href="#">Home</a>
+      <a href="#">About</a>
+      <a href="#">Contact</a>
+    </nav>
+  </header>
+</template>
