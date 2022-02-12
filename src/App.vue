32de6dd2771e5a3a7b4f244a6df94892a1eb3d6a
utshaweu <utshaw1995@gmail.com>
2022-02-12 19:14:01 +0600

    props binding done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
-  <div class="p-10">
-    <h1 class="text-3xl font-bold">Hello vue js</h1>
-    <h1 class="font-medium">Count: {{ count }}</h1>
-    <button
-      @click="count++"
-      class="bg-violet-500 hover:bg-violet-400 active:bg-violet-600 focus:outline-none focus:ring focus:ring-violet-300 px-5 py-2 mt-2 mr-2 rounded-3xl text-white font-bold"
-    >
-      Addition
-    </button>
-    <button
-      @click="count--"
-      class="bg-sky-600 hover:hover:bg-sky-700  active:hover:bg-sky-600  focus:outline-none focus:ring focus:hover:bg-sky-300 px-5 py-2 mt-2 rounded-3xl text-white font-bold"
-    >
-      Deduction
-    </button>
-  </div>
+  <Person name="Sajib" age="26"/>
 </template>
 
 <script>
+import Person from "./components/Person.vue";
 export default {
   name: "App",
+  components: {
+   Person, 
+  },
   data() {
     return {
-      count: 0,
+
     };
   },
 };
---
+++ src/components/Person.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="border border-dashed border-indigo-500 p-10 m-10">
+    <h1 class="text-4xl font-semibold">My name is {{name}}</h1>
+    <h1 class="text-2xl font-semibold">Age: {{age}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: ['name', 'age'],
+}
+</script>
