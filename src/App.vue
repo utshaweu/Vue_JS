c75890d0c15e47acb908879912382497fe8c4801
utshaweu <utshaw1995@gmail.com>
2022-02-12 20:40:43 +0600

    click event added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
   <Layout>
+
     <h1>Main Content</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt minus modi pariatur dolore tempora facere sint, corporis hic porro et ipsam magni iste. Minus perspiciatis ipsam debitis dolore voluptatum dolor est neque, quibusdam temporibus quas rerum nostrum at consequuntur quasi illo distinctio earum deserunt ullam itaque? Placeat numquam magnam quod.</p>
+      <p class="mb-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt minus modi pariatur dolore tempora facere sint, corporis hic porro et ipsam magni iste. Minus perspiciatis ipsam debitis dolore voluptatum dolor est neque, quibusdam temporibus quas rerum nostrum at consequuntur quasi illo distinctio earum deserunt ullam itaque? Placeat numquam magnam quod.</p>
+      <a :href="url" class="bg-indigo-500 text-white py-2 px-4">Click Here</a><br/><br/>
+
+      <button @click="toggle" class="text-white bg-blue-600 hover:bg-blue-500 py-2 px-4 mr-3">toggle</button>
+
+      <Person :name="name" :age="age" v-if="visible"/>
+
       <template #sidebar-left>
         <h1>Sidebar Left</h1>
       </template>
+
       <template #sidebar-right>
         <h1>Sidebar Right</h1>
       </template>
+
   </Layout>
 </template>
 
@@ -22,8 +31,16 @@ export default {
   },
   data() {
     return {
-
+      url: "https://google.com",
+      name: "Sajib",
+      age: 26,
+      visible: false,
     };
   },
+  methods: {
+    toggle(){
+      this.visible = !this.visible;
+    }
+  }
 };
 </script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="border border-dashed border-indigo-500 p-10 m-10">
+  <div class="border border-dashed border-indigo-500 p-10 mt-4">
     <h1 class="text-4xl font-semibold">My name is {{name}}</h1>
     <h1 class="text-2xl font-semibold">Age: {{age}}</h1>
     <slot/>
