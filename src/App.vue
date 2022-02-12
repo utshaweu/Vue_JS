bc09ece1c62cc91cc55b8cf4b2dde299b48250b9
utshaweu <utshaw1995@gmail.com>
2022-02-12 19:17:28 +0600

    slot is similarly react children type

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <Person name="Sajib" age="26"/>
+  <Person name="Sajib" age="26">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit ipsa quia, iste veniam earum id vel esse pariatur assumenda, quas minima magnam! Nobis quia maxime pariatur ad corrupti porro modi vel incidunt rem iusto ullam tempora ratione nemo laboriosam quis, soluta eveniet fuga iste laborum? Maiores dolor vel doloremque voluptatibus.</p>
+  </Person>
 </template>
 
 <script>
--- src/components/Person.vue
+++ src/components/Person.vue
@@ -2,6 +2,7 @@
   <div class="border border-dashed border-indigo-500 p-10 m-10">
     <h1 class="text-4xl font-semibold">My name is {{name}}</h1>
     <h1 class="text-2xl font-semibold">Age: {{age}}</h1>
+    <slot/>
   </div>
 </template>
 
